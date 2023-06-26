27bdcf6edc00adcc5feb465f2b5cc5fe48497aa0
Kennnnn <109525291+ant1417v3@users.noreply.github.com>
2023-06-26 23:21:11 +1000

    update Css and create box

--- quizz-app/src/App.jsx
+++ quizz-app/src/App.jsx
@@ -6,15 +6,15 @@ function App() {
 
   return (
     <main>
+      <div className='container'>       
+        <h2> First question</h2>
+        <button>First answer</button>
+        <h2> Second question</h2>
+        <h2> Third question</h2>
+        <h2> Fourth question</h2>
+        <h2> Fifth question</h2>
+      </div>
   
-      <h2> First question</h2>
-      <button>First answer</button>
-      <h2> Second question</h2>
-      <h2> Third question</h2>
-      <h2> Fourth question</h2>
-      <h2> Fifth question</h2>
-      
-      
     </main>
     
     
