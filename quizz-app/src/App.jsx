a35036c6663428dbe6ae621ecb6692300d93ad22
Kennnnn <109525291+ant1417v3@users.noreply.github.com>
2023-06-26 23:04:06 +1000

    Create space of answer and quesion

--- quizz-app/src/App.jsx
+++ quizz-app/src/App.jsx
@@ -2,17 +2,26 @@
 import './App.css'
 
 function App() {
+
+  async function fetchData() {
+    const response = await fetch("https://opentdb.com/api.php?amount=10&category=15");
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
+
   
 
   return (
     <main>
-  
+
+      <div className='contain'>
       <h2> First question</h2>
       <button>First answer</button>
       <h2> Second question</h2>
       <h2> Third question</h2>
       <h2> Fourth question</h2>
       <h2> Fifth question</h2>
+      </div>
       
       
     </main>
