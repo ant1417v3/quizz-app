1c939bc67812dd5bccdc97746cd485206d238938
Kennnnn <109525291+ant1417v3@users.noreply.github.com>
2023-06-27 18:13:59 +1000

    Update App.jsx
    Write function

--- quizz-app/src/App.jsx
+++ quizz-app/src/App.jsx
@@ -1,18 +1,37 @@
 
 import './App.css'
+import React from 'react'
 
 function App() {
+
+
+  const [count, setCount] =  React.useState(0)
+  const [dataQuizz, setDataQuizz] = React.useState([])
+  const [quizz, setQuizz] = React.useState({})
+
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10")
+    .then(res => res.json())
+    .then(data => setDataQuizz(data.results))
+
+  },[])
   
+  function getCategory(){
+    const category = dataQuizz[count]
+    setCount(prevCount => prevCount +1)
+    setQuizz(category)
+    
+  }
+ 
 
   return (
     <main>
       <div className='container'>       
-        <h2> First question</h2>
-        <button>First answer</button>
-        <h2> Second question</h2>
-        <h2> Third question</h2>
-        <h2> Fourth question</h2>
-        <h2> Fifth question</h2>
+                
+        <h2> {count}</h2>
+        <button onClick={getCategory}> Increase</button>
+        <h2>{quizz.question} </h2>
       </div>
   
     </main>
