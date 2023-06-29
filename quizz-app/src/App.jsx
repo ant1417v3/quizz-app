88c013398ad01aeaf37db1edb0c4a6a68ab9a6e8
Kennnnn <109525291+ant1417v3@users.noreply.github.com>
2023-06-30 02:12:36 +1000

    Fix function

--- quizz-app/src/App.jsx
+++ quizz-app/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import React from 'react'
 import QandA from './QandA'
 import {nanoid} from 'nanoid'
+import {decode} from 'html-entities'
 
 
 function App() {
@@ -10,26 +11,49 @@ function App() {
 
 
   const [dataQuizz, setDataQuizz] = React.useState([])
-  const [quizzs, setQuizz] = React.useState({})
+  const[questions, setQuestions] =React.useState([])
+
+  const [answers, setAnswers] = React.useState({
+    correct_answer:"",
+    all_answers:[],
+  })
+  
 
 
   React.useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=10")
     .then(res => res.json())
     .then(data => setDataQuizz(data.results))   
-    
 
   },[])
+
+  function getAllQuestions(array) {
+    const allquestions = [];
+    for (let i = 0; i < array.length; i++) {
+      allquestions[i] = array[i].question
+      
+    }
+    return allquestions
+  }
+
+  function startGame(){
+    const newQuestions = getAllQuestions(dataQuizz)
+    setQuestions(newQuestions) 
+  }
 
   console.log(dataQuizz)
 
 
-  const QuestionElement = dataQuizz.map(quizz =>
-  <QandA key={nanoid()} quizz={quizz}></QandA>)
+  // const QuestionElement = dataQuizz.map(quizz =>
+  // <QandA key={nanoid()} quizz={quizz} ></QandA>)
+
+  const QuestionElement = questions.map(question =>
+    <h2 key={nanoid()}>{decode(question)}</h2>)
 
   return (
     <main>    
       <div className='container'>   
+        <button  onClick={startGame}> Start Game</button>
         {QuestionElement}
       </div> 
     </main>
